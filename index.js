65fd4f45038ee0dbc87a3c1651f30480e504fcd5
Ishani Chettri <qtish13@dhcp-10-105-19-82.wireless.northwestern.private>
2019-07-23 23:33:54 -0500

    all changes

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-<button type="button" onclick="document.getElementById('demo').style.display='block'">
-Click Me!</button>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
